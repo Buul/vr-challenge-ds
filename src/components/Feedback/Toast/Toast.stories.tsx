2d4c3aacfa83c5b9958833c89ea48007650e1622
Paulo Silva <paulo.firmino2@gmail.com>
2024-07-08 16:52:47 -0300

    feat(Component): add Toast Component.

---
+++ src/components/Feedback/Toast/Toast.stories.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { Meta, StoryFn } from '@storybook/react';
+
+import { Button } from '@/components/Form';
+
+import {
+  Canvas,
+  Container,
+  Row,
+  Title,
+} from '../../../utils/StoriesComponents';
+
+import { ToastVariants } from './Toast.types';
+import { Toast as ToastComponent, ToastProps } from '.';
+
+const meta: Meta = {
+  title: '🧩 Components/Feedbacks/Toast',
+  component: ToastComponent,
+  argTypes: {},
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+const argsProps: ToastProps = { variant: 'success' };
+
+export default meta;
+
+const Template: StoryFn<ToastProps> = args => {
+  const [toastVariant, setToastVariant] = useState<ToastVariants>('success');
+
+  const message = {
+    success: 'Mensagem sucesso!',
+    error: 'Mensagem erro!',
+    loading: 'Mensagem carregando!',
+  };
+
+  const handleClick = (type: ToastVariants) => {
+    setToastVariant(type);
+    toast(message[type], { id: type });
+  };
+  return (
+    <Canvas>
+      <Title> Toast (success, error, loading)</Title>
+      <Container>
+        <div style={{ display: 'flex', width: '100%', marginLeft: 20 }}>
+          <Button
+            onClick={() => handleClick('success')}
+            style={{ backgroundColor: '#BCD952', marginRight: '20px' }}
+          >
+            Success
+          </Button>
+          <Button
+            onClick={() => handleClick('error')}
+            style={{ backgroundColor: '#A20000', marginRight: '20px' }}
+          >
+            Error
+          </Button>
+          <Button
+            onClick={() => handleClick('loading')}
+            style={{ backgroundColor: '#228BE6', marginRight: '20px' }}
+          >
+            Loading
+          </Button>
+        </div>
+        <Row>
+          <ToastComponent {...args} variant={toastVariant} id={toastVariant} />
+        </Row>
+      </Container>
+      <Container style={{ marginTop: 50 }}>
+        <Title>
+          This Toast use the <strong>react-hot-toast</strong>, see documentation
+          at the link below
+        </Title>
+      </Container>
+      <Container>
+        <Title>
+          <a
+            href="https://react-hot-toast.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://react-hot-toast.com/
+          </a>
+        </Title>
+      </Container>
+    </Canvas>
+  );
+};
+
+export const Default = Template.bind({});
+
+Default.parameters = { options: { showPanel: true } };
+
+Default.args = { ...argsProps };
---
+++ src/components/Feedback/Toast/Toast.tsx
@@ -0,0 +1,90 @@
+import { FC, useEffect, useState } from 'react';
+import toast, { ToastBar, Toaster } from 'react-hot-toast';
+
+import { Loading } from '../Loading';
+
+import * as Styled from './Toast.styled';
+import { ToastProps } from './Toast.types';
+
+const icon = {
+  success: (
+    <Styled.Icon>
+      <img alt="ícone sucesso" src="/icons/toast-img-success.svg" />
+    </Styled.Icon>
+  ),
+  error: (
+    <Styled.Icon>
+      <img alt="ícone erro" src="/icons/toast-img-error.svg" />
+    </Styled.Icon>
+  ),
+  loading: (
+    <Styled.Icon>
+      <Loading height={22} width={22} />
+    </Styled.Icon>
+  ),
+};
+
+const ToasterComponent: FC<ToastProps> = ({
+  variant,
+  position = 'bottom-center',
+  duration = 3000,
+  id,
+}) => {
+  const [previousId, setPreviousId] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (previousId) {
+      toast.remove(previousId);
+    }
+    if (id) {
+      setPreviousId(id);
+    }
+  }, [id]);
+
+  const handleClose = () => {
+    toast.remove();
+  };
+
+  return (
+    <Styled.ToasterWrapper>
+      <Toaster
+        position={position}
+        toastOptions={{
+          className: '',
+          duration,
+          style: {
+            width: '100%',
+            color: '#545556',
+            boxShadow:
+              '0px 7px 7px -5px rgba(0, 0, 0, 0.04), 0px 10px 15px -5px rgba(0, 0, 0, 0.10), 0px 1px 3px 0px rgba(0, 0, 0, 0.05)',
+            backgroundColor: '#ffffff',
+            padding: '0 8px',
+            minHeight: 56,
+            fontSize: 14,
+            fontStyle: 'normal',
+            border: '1px solid  #E7E7E7',
+            fontWeight: 400,
+            textAlign: 'left',
+            marginRight: 0,
+            borderRadius: 4,
+          },
+        }}
+      >
+        {t => (
+          <ToastBar toast={t}>
+            {({ message }) => (
+              <Styled.ToastWrapper>
+                {icon[variant]}
+                <Styled.MessageWrapper>{message}</Styled.MessageWrapper>
+                <Styled.CloseButton onClick={handleClose}>
+                  <img alt="ícone com letra X" src="/icons/close.svg" />
+                </Styled.CloseButton>
+              </Styled.ToastWrapper>
+            )}
+          </ToastBar>
+        )}
+      </Toaster>
+    </Styled.ToasterWrapper>
+  );
+};
+export default ToasterComponent;
